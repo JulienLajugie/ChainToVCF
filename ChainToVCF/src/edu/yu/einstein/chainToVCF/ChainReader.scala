94c2cb3e8a3b0a14b4b6adbd24f622863dc70328
JulienLajugie <julien.lajugie@gmail.com>
2014-05-09 17:30:28 -0400

    implementation in progress

--- ChainToVCF/src/edu/yu/einstein/chainToVCF/ChainReader.scala
+++ ChainToVCF/src/edu/yu/einstein/chainToVCF/ChainReader.scala
@@ -1,61 +1,102 @@
+/*******************************************************************************
+ * ChainToVCF
+ * Copyright (C) 2014 Albert Einstein College of Medicine
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Authors: Julien Lajugie <julien.lajugie@einstein.yu.edu>
+ *
+ * Website: <https://github.com/JulienLajugie/ChainToVCF>
+ ******************************************************************************/
 package edu.yu.einstein.chainToVCF
 
 import scala.io.Source
-import java.awt.SplashScreen
+import scala.collection.immutable.HashSet
 
 class ChainReader(chainFilePath: String) {
-  
+
   val chainLines = Source.fromFile(chainFilePath).getLines
-  
-  def printChainFileAsVCF() {
-    def chainFileToVCF() {
+
+  def printChainAsVCF(chaineLine: String, extractedChr: HashSet[String]): HashSet[String] = {
+    // extract a chain and print it
+    def extractChainContent(chromo: String, position: Int): Unit = {
       if (chainLines.hasNext) {
         val line = chainLines.next
-        if (line.trim().startsWith("chain")) printChainAsVCF(line)
-        chainFileToVCF
-      }
-    }
-    
-    def printChainAsVCF(chaineLine: String) {
-
-      // extract a chain and print it with 
-      def extractChainContent(chromo: String, position: Int) {
-        if (chainLines.hasNext) {
-          val line = chainLines.next
-          val splitLine = line.split("\t")
-          if (splitLine.length == 3) {
-            val varPos = position + Integer.parseInt(splitLine(0))
-            val tPos = Integer.parseInt(splitLine(1))
-            val qPos = Integer.parseInt(splitLine(2))
-            if (tPos > qPos) VCFWriter.printVariant(new Deletion(chromo, varPos, tPos - qPos))
-            else if (tPos < qPos) VCFWriter.printVariant(new Insertion(chromo, varPos, qPos - tPos))
-            else VCFWriter.printVariant(new SNP(chromo, varPos + 1, 'N', 'N'))
-            extractChainContent(chromo, varPos + tPos)
+        val splitLine = line.split("\t")
+        if (splitLine.length == 3) {
+          val varPos = position + Integer.parseInt(splitLine(0))
+          val tPos = Integer.parseInt(splitLine(1))
+          val qPos = Integer.parseInt(splitLine(2))
+          if (tPos > qPos) {
+            VCFWriter.printVariant(new Deletion(chromo, varPos, tPos - qPos))
+          } else if (tPos < qPos) {
+            VCFWriter.printVariant(new Insertion(chromo, varPos, qPos - tPos))
+          } else {
+            VCFWriter.printVariant(new SNP(chromo, varPos + 1, 'N', 'N'))
           }
+          extractChainContent(chromo, varPos + tPos)
         }
       }
-      
-      val splitLine = chaineLine.split(" ")
-      val tChr = splitLine(2)
-      val qChr = splitLine(7)
-      val tStrand = splitLine(4)
-      val qStrand = splitLine(9)
-      if (tChr.equals(qChr) && tStrand.equals(qStrand)) {
-        val tPos = Integer.parseInt(splitLine(5))
-        val qPos = Integer.parseInt(splitLine(10))
-        if (tPos > qPos) VCFWriter.printVariant(new Deletion(tChr, tPos, tPos - qPos))
-        else if (tPos < qPos) VCFWriter.printVariant(new Insertion(tChr, tPos, qPos - tPos))
-        extractChainContent(tChr, tPos)
     }
+
+    val splitLine = chaineLine.split(" ")
+    val tChr = splitLine(ChainReader.refChr)
+    if (!extractedChr.contains(tChr)) {
+      val qChr = splitLine(ChainReader.queryChr)
+      val tStrand = splitLine(ChainReader.refStrand)
+      val qStrand = splitLine(ChainReader.queryStrand)
+      if (tChr.equals(qChr) && tStrand.equals(qStrand)) {
+        val tPos = Integer.parseInt(splitLine(ChainReader.refPos))
+        val qPos = Integer.parseInt(splitLine(ChainReader.queryPos))
+        if (tPos > qPos) {
+          VCFWriter.printVariant(new Deletion(tChr, tPos, tPos - qPos))
+        } else if (tPos < qPos) {
+          VCFWriter.printVariant(new Insertion(tChr, tPos, qPos - tPos))
+        }
+        extractChainContent(tChr, tPos)
+      }
+      extractedChr
+    }
+    extractedChr + tChr
   }
-    
+
+  def printChainFileAsVCF(): Unit = {
+    def chainFileToVCF(extractedChr: HashSet[String]): Unit = {
+      if (chainLines.hasNext) {
+        val line = chainLines.next
+        if (line.trim().startsWith("chain")) {
+          chainFileToVCF(printChainAsVCF(line, extractedChr))
+        } else {
+          chainFileToVCF(extractedChr)
+        }
+      }
+    }
     VCFWriter.printVCFHeader
-    chainFileToVCF
-  } 
+    chainFileToVCF(new HashSet[String])
+  }
 }
 
+
 object ChainReader {
-    def main(args: Array[String]): Unit = {
-      new ChainReader("/home/jlajugie/Downloads/hg38ToHg19.chr1.over.chain").printChainFileAsVCF
-    }
+
+  val refChr        = 2     // chromosome (reference sequence) column in a chain file
+  val refStrand     = 4     // strand (reference sequence) column in a chain file
+  val refPos        = 5     // alignment start position (reference sequence) column in a chain file
+  val queryChr      = 7     // chromosome (query sequence) column in a chain file
+  val queryStrand   = 9     // strand (query sequence) column in a chain file
+  val queryPos      = 10    // alignment start position (query sequence) column in a chain file
+
+  def main(args: Array[String]): Unit = {
+    new ChainReader("/home/jlajugie/Downloads/hg38ToHg19.over.chain").printChainFileAsVCF
+  }
 }
--- ChainToVCF/src/edu/yu/einstein/chainToVCF/VCFWriter.scala
+++ ChainToVCF/src/edu/yu/einstein/chainToVCF/VCFWriter.scala
@@ -1,6 +1,27 @@
+/*******************************************************************************
+ * ChainToVCF
+ * Copyright (C) 2014 Albert Einstein College of Medicine
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Authors: Julien Lajugie <julien.lajugie@einstein.yu.edu>
+ *
+ * Website: <https://github.com/JulienLajugie/ChainToVCF>
+ ******************************************************************************/
 package edu.yu.einstein.chainToVCF
 
 object VCFWriter {
+  private val columnDelimiter = "\t";
   private val defaultID = "."
   private val defautlRef = "N"
   private val insertionTag = "<INS>"
@@ -11,7 +32,7 @@ object VCFWriter {
   private val defaultSNPInfoPattern = "END=%d;SVLEN=1"
   private val defaultFormatField = "GT"
   private val defaultGTField = "1/1"
-  private val header = 
+  private val header =
     "##fileformat=VCFv4.0\n" +
     "##fileDate=June 08, 2011\n" +
     "##reference=NCBI38-HG38\n" +
@@ -22,32 +43,32 @@ object VCFWriter {
     "##INFO=<ID=END,Number=1,Type=Integer,Description=\"End position of the variant described in this record\">\n" +
     "##INFO=<ID=SVLEN,Number=-1,Type=Integer,Description=\"Difference in length between REF and ALT alleles\">\n" +
     "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n" +
-    "#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	hg19";
-  
+    "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\thg19";
+
   private def getVCFLine(variant: Variant): String = {
     variant match {
       case Insertion(chromo, pos, length) =>
-        chromo + "\t" + pos + "\t" + defaultID + "\t" + defautlRef + "\t" + 
-        insertionTag + "\t" + defaultQualityField + "\t" + defaultFilterField + "\t" +
-        defaultSVInfoPattern.format("INS", pos, length) + "\t" + defaultFormatField + "\t" + defaultGTField
+        chromo + columnDelimiter + pos + columnDelimiter + defaultID + columnDelimiter + defautlRef + columnDelimiter +
+        insertionTag + columnDelimiter + defaultQualityField + columnDelimiter + defaultFilterField + columnDelimiter +
+        defaultSVInfoPattern.format("INS", pos, length) + columnDelimiter + defaultFormatField + columnDelimiter + defaultGTField
       case Deletion(chromo, pos, length) =>
-        chromo + "\t" + pos + "\t" + defaultID + "\t" + defautlRef + "\t" + 
-        deletionTag + "\t" + defaultQualityField + "\t" + defaultFilterField + "\t" +
-        defaultSVInfoPattern.format("DEL", pos + length, -length) + "\t" + defaultFormatField + "\t" + defaultGTField
+        chromo + columnDelimiter + pos + columnDelimiter + defaultID + columnDelimiter + defautlRef + columnDelimiter +
+        deletionTag + columnDelimiter + defaultQualityField + columnDelimiter + defaultFilterField + columnDelimiter +
+        defaultSVInfoPattern.format("DEL", pos + length, -length) + columnDelimiter + defaultFormatField + columnDelimiter + defaultGTField
       case SNP(chromo, pos, ref, alt) =>
-        chromo + "\t" + pos + "\t" + defaultID + "\t" + ref + "\t" + 
-        alt + "\t" + defaultQualityField + "\t" + defaultFilterField + "\t" +
-        defaultSNPInfoPattern.format(pos) + "\t" + defaultFormatField + "\t" + defaultGTField
+        chromo + columnDelimiter + pos + columnDelimiter + defaultID + columnDelimiter + ref + columnDelimiter +
+        alt + columnDelimiter + defaultQualityField + columnDelimiter + defaultFilterField + columnDelimiter +
+        defaultSNPInfoPattern.format(pos) + columnDelimiter + defaultFormatField + columnDelimiter + defaultGTField
     }
   }
-  
-  
-  def printVCFHeader() {
+
+
+  def printVCFHeader(): Unit = {
     println(header)
   }
-  
-  
-  def printVariant(variant: Variant) {
+
+
+  def printVariant(variant: Variant): Unit = {
     println(getVCFLine(variant))
   }
 }
